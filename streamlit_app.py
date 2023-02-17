d59c32e3821318c33fe4b22bbb890959034b5363
Edmund Neil <edmondneil@gmail.com>
2023-02-17 15:10:46 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@
 import streamlit
 streamlit.title('My Parents Healthy Diner')
 
-streamlit.header('Breakfast Favorites")
+streamlit.header('Breakfast Favorites')
 streamlit.text(' Omega 3 & Blueberry Oatmeal') 
 streamlit.text ('© Kale, Spinach & Rocket Smoothie') 
 streamlit.text ('< Hard-Boiled Free-Range Egg')
